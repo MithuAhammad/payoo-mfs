c222a7b93a4f8679cdccd680c0f8a36b1e352e9f
MithuAhammad <mithuahammad2@gmail.com>
2024-12-06 08:55:21 +0600

    added login ui

--- js/login.js
+++ js/login.js
@@ -1,3 +1,15 @@
-document.getElementById('btn-login').addEventListener('click',function(){
-    
-})
+document
+  .getElementById("btn-login")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); //vejal to beginners
+    const mobileNumber = document.getElementById("mobile-number").value;
+    const pinNumber = document.getElementById("pin-number").value;
+    // console.log(mobileNumber, pinNumber);
+    // bad way to validate
+    if (mobileNumber === "5" && pinNumber === "1234") {
+      console.log("you are login");
+      window.location.href='/home.html'
+    } else {
+      alert("wrong phone & pin ");
+    }
+  });
